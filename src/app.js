3b5630948e828d762d3cf63e33231817d584b877
Amar Biradar <ambir0513@gmail.com>
2025-03-17 12:47:32 +0530

    cors added in dependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import auth from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/", auth);
 
 export default app;
